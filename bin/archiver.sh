ccfa02124012b7d9039a7532c9ac1939a1b7c8de
Guy Cole <guycole@gmail.com>
2026-08-26 08:48:19 -0700

    archive script

--- bin/archiver.sh
+++ bin/archiver.sh
@@ -7,13 +7,15 @@
 #
 PATH=/bin:/usr/bin:/etc:/usr/local/bin; export PATH
 #
+HOST_NAME=$(hostname)
 TODAY=$(date '+%Y-%m-%d')
-FILE_NAME="capybara-${TODAY}.tgz"
+FILE_NAME="${HOST_NAME}-${TODAY}.tgz"
 #
+ARCHIVE_DIR="archive"
 EXPORT_DIR="export"
 SOURCE_DIR="capybara"
 SUCCESS_DIR="success"
-WORK_DIR="/var/wombat/fresh"
+WORK_DIR="/var/wombat/capybara"
 #
 echo "start archiver" 
 #
@@ -22,8 +24,13 @@ cd ${WORK_DIR}
 mv ${SUCCESS_DIR} ${SOURCE_DIR}
 mkdir ${SUCCESS_DIR}
 #
-tar -cvzf ${FILE_NAME} ${SOURCE_DIR}
-mv ${FILE_NAME} ${EXPORT_DIR}
+# archive everything
+tar -cvzf "${ARCHIVE_DIR}/${FILE_NAME}" ${SOURCE_DIR}
+#
+# export only collector files
+rm "${SOURCE_DIR}/acars*.json"
+rm "${SOURCE_DIR}/vdl2*.json"
+tar -cvzf "${EXPORT_DIR}/${FILE_NAME}" ${SOURCE_DIR}/*.json
 #
 echo "cleanup"
 rm -rf ${SOURCE_DIR}
